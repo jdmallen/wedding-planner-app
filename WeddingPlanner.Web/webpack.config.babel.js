7234f2180ec80dbb18f4705e22ef44d9f474b3d6
Jesse Mallen <lightandtheheat@gmail.com>
2018-03-10 16:48:11 -0500

    Add 2 npm packages, fix typo in webpack config

--- WeddingPlanner.Web/webpack.config.babel.js
+++ WeddingPlanner.Web/webpack.config.babel.js
@@ -4,36 +4,38 @@ import path from "path";
 const BUILD_DIR = path.resolve(__dirname, "wwwroot");
 const APP_DIR = path.resolve(__dirname, "ClientApp");
 
-const isMinimized = process.argv.indexOf("--optimize-minimize") > -1 || process.argv.indexOf("-p") > -1;
+const isMinimized =
+  process.argv.indexOf("--optimize-minimize") > -1 ||
+  process.argv.indexOf("-p") > -1;
 
 const config = {
-	entry: `${APP_DIR}/index.jsx`,
-	output: {
-		path: BUILD_DIR,
-		filename: isMinimized ? "jd/app.min.js" : "js/app.js",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				loader: "babel-loader",
-				query: {
-					presets: ["env", "react"],
-				},
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: "eslint-loader",
-			},
-		],
-	},
-	resolve: {
-		extensions: [".js", ".jsx"],
-	},
-	externals: [],
-	plugins: [],
+  entry: `${APP_DIR}/index.jsx`,
+  output: {
+    path: BUILD_DIR,
+    filename: isMinimized ? "js/app.min.js" : "js/app.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["env", "react"]
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  externals: [],
+  plugins: []
 };
 
 export default config;
