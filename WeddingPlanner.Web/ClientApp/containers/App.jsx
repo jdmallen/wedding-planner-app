b05fec493b80f2064984f86f27df8f4e486b2b61
Jesse Mallen <lightandtheheat@gmail.com>
2018-03-13 23:15:57 -0400

    Get CSS modules to work properly

--- WeddingPlanner.Web/ClientApp/containers/App.jsx
+++ WeddingPlanner.Web/ClientApp/containers/App.jsx
@@ -1,17 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import styles from "./App.scss";
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src="img/logo.svg" className="App-logo" alt="logo" />
-				<h1 className="App-title">Welcome to React</h1>
-			</header>
-			<p className="App-intro">
-				To get started, edit <code>src/App.js</code> and save to reload.
-			</p>
-		</div>
-	);
+class App extends Component {
+	constructor() {
+		super();
+		this.state = { count: 0 };
+	}
+
+	increment() {
+		this.setState({
+			count: this.state.count + 1,
+		});
+	}
+
+	render() {
+		return (
+			<div className={styles.app}>
+				<header className={styles.appHeader}>
+					<img src="img/logo.svg" className={styles.appLogo} alt="logo" />
+					<h1 className={styles.appTitle}>Welcome to React</h1>
+				</header>
+				<p className={styles.appIntro}>
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+			</div>
+		);
+	}
 }
 
 export default App;
--- WeddingPlanner.Web/webpack.config.babel.js
+++ WeddingPlanner.Web/webpack.config.babel.js
@@ -1,5 +1,5 @@
-import webpack from "webpack";
 import path from "path";
+import ExtractTextPlugin from "extract-text-webpack-plugin";
 
 const BUILD_DIR = path.resolve(__dirname, "wwwroot");
 const APP_DIR = path.resolve(__dirname, "ClientApp");
@@ -9,33 +9,46 @@ const isMinimized =
   process.argv.indexOf("-p") > -1;
 
 const config = {
-  entry: `${APP_DIR}/index.jsx`,
-  output: {
-    path: BUILD_DIR,
-    filename: isMinimized ? "js/app.min.js" : "js/app.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          presets: ["env", "react"]
-        }
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader"
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  externals: [],
-  plugins: []
+	entry: `${APP_DIR}/index.jsx`,
+	output: {
+		path: BUILD_DIR,
+		filename: isMinimized ? "js/[name].bundle.min.js" : "js/[name].bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				use: [
+					"babel-loader",
+					"eslint-loader"
+				],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: [
+						{
+							loader: "css-loader",
+							options: {
+								modules: true,
+								localIdentName: "[name]__[local]___[hash:base64:5]"
+							}
+						},
+						"sass-loader"
+					]
+				})
+			},
+		]
+	},
+	resolve: {
+		extensions: [".js", ".jsx"]
+	},
+	externals: [],
+	plugins: [
+		new ExtractTextPlugin("css/[name].css", { allChunks: true })
+	]
 };
 
 export default config;
