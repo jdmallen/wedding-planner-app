b8f242956f597c41797b32ad6c25071213ef1138
J.D. Mallen <lightandtheheat@gmail.com>
2019-03-18 18:36:46 -0400

    Fix db name, bundle ref

--- WeddingPlanner.Web/Controllers/BaseViewController.cs
+++ WeddingPlanner.Web/Controllers/BaseViewController.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WeddingPlanner.Web.Controllers
 {
 	[Route("")]
 	public class BaseViewController : Controller
 	{
+		private readonly IHostingEnvironment _hostingEnvironment;
+
+		public BaseViewController(IHostingEnvironment hostingEnvironment)
+		{
+			_hostingEnvironment = hostingEnvironment;
+		}
+
 		public IActionResult Index()
 		{
-			return View();
+			return View(
+				_hostingEnvironment.EnvironmentName.IndexOf(
+					"development",
+					StringComparison.CurrentCultureIgnoreCase)
+				!= -1);
 		}
 	}
 }
